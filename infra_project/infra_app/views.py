6d7e765dc19a0a007954238e77a2f385bdff3e5c
Aleksandr <haus.esc@gmail.com>
2022-06-27 01:12:21 +0300

    add test with flake8_v3

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -1,9 +1,9 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse('У меня получилось!')
+    return render(request, 'У меня получилось!')
 
 
 def second_page(request):
-    return HttpResponse('А это вторая страница')
+    return render(request, 'А это вторая страница')
